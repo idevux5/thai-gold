b5684b007d69f1dec59841072d429c740f6a6eb6
idevux5 <idevux5@gmail.com>
2025-09-30 20:28:15 +0700

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,10 @@
-import fastify, {
-  FastifyServerOptions,
-  FastifyRequest,
-  FastifyReply,
-} from "fastify";
-import fastifyCors from "fastify-cors";
-import priceRouters from "./routers/price";
-import track from "./plugins/track";
+import Fastify from "fastify";
+import priceRoutes from "./routers/price";
 
-const buildApp = (options: FastifyServerOptions) => {
-  const app = fastify(options);
+export default function buildApp(options = {}) {
+  const app = Fastify(options);
 
-  app.register(fastifyCors, {
-    origin: "*",
-  });
-
-  app.register(track, {});
-
-  app.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
-    reply.code(200).send({
-      status: "success",
-      response:
-        "Please go to https://github.com/max180643/thai-gold-api for API usage.",
-    });
-  });
-
-  app.register(priceRouters, { prefix: "/" });
-
-  app.get("*", async (request: FastifyRequest, reply: FastifyReply) => {
-    reply.code(404).send({
-      status: "failure",
-      response: "route not found.",
-    });
-  });
+  app.register(priceRoutes);
 
   return app;
-};
-
-export default buildApp;
+}
