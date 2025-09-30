705a0dd44921971d51e69665538d15fcfbfcd9fd
idevux5 <idevux5@gmail.com>
2025-09-30 20:07:32 +0700

    Move project files to root

--- src/routers/price.ts
+++ src/routers/price.ts
@@ -1,40 +1,8 @@
-import fetch from "node-fetch";
+import express from 'express';
+const router = express.Router();
 
-interface GoldPrice {
-  gold_buy: string;
-  gold_sell: string;
-  gold_date: string;
-}
+router.get('/price', (req, res) => {
+  res.json({ gold: 'ราคาทองวันนี้' });
+});
 
-const priceHandler = async () => {
-  try {
-    // เรียก API ไทยทอง
-    const res = await fetch("https://thai-gold-api-1.vercel.app/latest");
-    const data = await res.json();
-
-    if (data.status === "success") {
-      const latest: GoldPrice = data.response;
-
-      return {
-        status: "success",
-        response: {
-          buy: latest.gold_buy,
-          sell: latest.gold_sell,
-          date: latest.gold_date,
-        },
-      };
-    } else {
-      return {
-        status: "error",
-        response: "ไม่สามารถดึงราคาทองได้",
-      };
-    }
-  } catch (err) {
-    return {
-      status: "error",
-      response: "เกิดข้อผิดพลาดในการดึงข้อมูล",
-    };
-  }
-};
-
-export default priceHandler;
+export default router;
